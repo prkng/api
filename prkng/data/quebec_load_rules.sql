3c723de87aa3064c2d7de209e59dbeb9a6b32560
Ludovic Delauné <ludovic.delaune@oslandia.com>
2015-02-10 20:41:33 +0100

    add missing files for quebec

---
+++ prkng/data/quebec_load_rules.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS quebec_rules_translation;
+CREATE TABLE quebec_rules_translation (
+    id serial,
+    code varchar,
+    description varchar,
+    season_start varchar,
+    season_end varchar,
+    time_max_parking float,
+    time_start float,
+    time_end float,
+    time_duration float,
+    lun smallint,
+    mar smallint,
+    mer smallint,
+    jeu smallint,
+    ven smallint,
+    sam smallint,
+    dim smallint,
+    daily float,
+    special_days varchar,
+    restrict_typ varchar
+);
+
+copy quebec_rules_translation (code,description,season_start,season_end,
+    time_max_parking,time_start,time_end,time_duration,lun,mar,mer,jeu,ven,
+    sam,dim,daily,special_days,restrict_typ)
+from '{}'
+WITH CSV HEADER DELIMITER ',' ENCODING 'UTF-8';
---
+++ prkng/processing/filters.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+from itertools import chain
+from datetime import timedelta, datetime
+from collections import namedtuple, defaultdict
+from itertools import groupby
+from aniso8601 import parse_datetime
+
+
+def on_restriction(rules, checkin, duration):
+    """
+    Returns True if restrictions are consistent with the checkin
+    and duration given in argument. False otherwise
+
+    :param rules: list of rules (dict)
+    :param checkin: checkin time
+    :param duration: duration in hour. Float accepted
+    """
+    checkin = parse_datetime(checkin)
+    duration = timedelta(hours=duration)
+    checkin_end = checkin + duration  # datetime
+
+    month = checkin.date().month  # month as number
+    isodow = checkin.isoweekday()  # 1->7
+    year = checkin.year  # 2015
+    day = checkin.strftime('%d')  # 07
+
+    # analyze each rule and stop iteration on conflict
+    for rule in rules:
+
+        # first test season day/month
+        start_day, start_month = ('-' or rule['season_start']).split('-')
+        end_day, end_month = ('-' or rule['season_end']).split('-')
+        season_match = season_matching(
+            start_day,
+            start_month,
+            end_day,
+            end_month,
+            day,
+            month
+        )
+
+        if not season_match:
+            # not concerned, going to the next rule
+            continue
+
+        max_time_ok = True
+        time_range_ok = True
+
+        # analyze time_max_parking
+        time_max_parking = timedelta(minutes=rule['time_max_parking'] or 3679200)
+        if duration > time_max_parking:
+            max_time_ok &= False
+
+        # extract time range for each day and test overlapping with checkin + duration
+        # start at current day and slice over days
+        iterto = chain(range(1, 8)[isodow-1:], range(1, 8)[:isodow-1])
+
+        for absoluteday, numday in enumerate(iterto):
+            tsranges = rule['agenda'][str(numday)]
+            for start, stop in filter(bool, tsranges):
+
+                start_time = datetime(
+                    year, month, int(day)+absoluteday,
+                    hour=int(start), minute=int(start % 1 * 60))
+
+                #  hack to avoid ValueError: hour must be in 0..23
+                stop_time = datetime(
+                    year, month, int(day)+absoluteday,
+                    hour=int(stop-1), minute=int(stop % 1 * 60)) + timedelta(hours=1)
+
+                if max(start_time, checkin) < min(stop_time, checkin_end):
+                    # overlapping !
+                    time_range_ok &= False
+
+            if not max_time_ok or not time_range_ok:
+                # max_time exceed or time range overlapping or both
+                return True
+
+    return False
+
+
+def season_matching(start_day, start_month, end_day, end_month,
+                    day, month):
+    if not start_month:
+        # no season restriction so matching ok
+        return True
+
+    if start_month < end_month and not (month >= start_month and month <= end_month):
+        # month out of range
+        return False
+    if start_month > end_month and (month < start_month and month > end_month):
+        # month out of range
+        return False
+
+    if month == start_month:
+        if day < start_day:
+            return False
+
+    if month == end_month:
+        if day > end_day:
+            return False
+
+    return True
+
+
+def group_rules(rules):
+    """
+    group rules having the same code and contructs an array of
+    parking time for each day
+    """
+    singles = namedtuple('singles', (
+        'code', 'description', 'season_start', 'season_end',
+        'time_max_parking', 'agenda', 'special_days', 'restrict_typ'
+    ))
+
+    results = []
+    days = ('lun', 'mar', 'mer', 'jeu', 'ven', 'sam', 'dim')
+
+    for code, group in groupby(rules, lambda x: x.code):
+
+        day_dict = defaultdict(list)
+
+        for part in group:
+            for numday, day in enumerate(days, start=1):
+                isok = getattr(part, day) or part.daily
+                if not isok:
+                    continue
+                # others cases
+                if part.time_end:
+                    day_dict[numday].append([part.time_start, part.time_end])
+
+                elif part.time_duration:
+                    fdl, ndays, ldf = split_time_range(part.time_start, part.time_duration)
+                    # first day
+                    day_dict[numday].append([part.time_start, part.time_start + fdl])
+
+                    for inter_day in xrange(1, ndays + 1):
+                        day_dict[numday + inter_day].append([0, 24])
+                    # last day
+                    if ldf != 0:
+                        day_dict[numday].append([0, ldf])
+
+                else:
+                    day_dict[numday].append([0, 24])
+
+        # add empty days
+        for numday, day in enumerate(days, start=1):
+            if not day_dict[numday]:
+                day_dict[numday].append(None)
+
+        results.append(singles(
+            part.code,
+            part.description,
+            part.season_start,
+            part.season_end,
+            part.time_max_parking,
+            dict(day_dict),
+            part.special_days,
+            part.restrict_typ
+        ))
+
+    return results
+
+
+def split_time_range(start_time, duration):
+    """
+    Given a start time and a duration, returns a 3-tuple containing
+    the time left for the current day, a number of plain day left, a number of hours left
+    for the last day
+    """
+    if start_time + duration <= 24:
+        # end is inside the first day
+        return duration, 0, 0
+
+    time_left_first = 24 - start_time
+    plain_days = (duration - time_left_first) // 24
+    time_left_last = (duration - time_left_first) % 24
+    return time_left_first, int(plain_days), time_left_last
