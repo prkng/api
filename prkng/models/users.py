22957b665e10642de11d32e7b84de886e6f78896
Jacob Cook <jacob@peakwinter.net>
2015-09-17 11:37:33 -0400

    bugfix

--- prkng/models/users.py
+++ prkng/models/users.py
@@ -81,8 +81,8 @@ class User(UserMixin):
                     image_url=image_url or self.image_url
             )
         )
-        self.name = name.encode('utf-8') or self.name
-        self.email = email.encode('utf-8') or self.email
+        self.name = name.encode('utf-8') if name else self.name
+        self.email = email.encode('utf-8') if email else self.email
         self.gender = gender or self.gender
         self.image_url = image_url or self.image_url
 
@@ -125,6 +125,8 @@ class User(UserMixin):
         does exist then return a User Object.  If not then return None as
         required by Flask-Login.
         """
+        if not email:
+            return None
         res = user_table.select(user_table.c.email == email.lower()).execute().first()
         if not res:
             return None
